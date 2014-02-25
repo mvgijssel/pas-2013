3d26a5daba3d1ba035b616984e8263d5fe2343a3
paulluuk <meneerpluk@gmail.com>
2014-02-25 14:31:30 +0100

    verbeterde versie balherkenning

--- brain/src/behavior/plukkleur/balherkenning.py
+++ brain/src/behavior/plukkleur/balherkenning.py
@@ -148,6 +148,8 @@ class RasterImage:
             oldpic.set_at((midX,j),(255,255,255))
 
         # stuur foto naar window met naam "balhekenner"
+        cv2.namedWindow("Balherkenner")
+        cv2.moveWindow("Balherkenner",100,100)
         cv_image = pygame.surfarray.array2d(oldpic)
         cv2.imshow("Balherkenner", cv_image)
         cv2.waitKey(10)
