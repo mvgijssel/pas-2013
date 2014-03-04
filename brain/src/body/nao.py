34e4b0c1c5a2bc8eebd4e6258d221a8832ac4eb6
paulluuk <meneerpluk@gmail.com>
2014-03-04 10:31:02 +0100

    fix

--- brain/src/body/nao.py
+++ brain/src/body/nao.py
@@ -699,7 +699,7 @@ class Nao(object):
         # returned afstand van bal (in meters), maar werkt alleen als de bal in het midden van het blikveld ligt!
         # is niet nauwkeurig, alleen een indicatie. Moet worden getest om te zien wat de afwijking is.
         # afstand (cm) = tan(hoek) * 45cm (want nao camera is 45cm hoog)
-        hoekhoofd = self.get_angles(['HeadPitch'], True)
+        hoekhoofd = self.get_angles(['HeadPitch'], True)[0]
         dist = math.tan(hoekhoofd) * 0.45
         return dist
     def loop_naar_bal(self):
