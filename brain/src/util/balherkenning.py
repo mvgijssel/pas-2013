41f8df55d67949c80110e62f7ab7142228b2a8cb
paulluuk <meneerpluk@gmail.com>
2014-03-04 14:05:30 +0100

    verbeterde "loop naar bal"

--- brain/src/util/balherkenning.py
+++ brain/src/util/balherkenning.py
@@ -62,21 +62,21 @@ class RasterImage:
                 g = col.g
                 b = col.r
                 minwaarde = 50 # moet minimaal zoveel van de kleur aanwezig zijn <0,255>, om zwart uit te schakelen
-                factor = 1.1 # er moet minimaal "factor" keer zoveel "kleur" zijn als andere kleuren samen
+                factor = 0.9 # er moet minimaal "factor" keer zoveel "kleur" zijn als andere kleuren samen
                 maxwaarde = 300 # de andere kleuren samen mogen maximaal deze waarde hebben, om wit uit te schakelen
                 if (color == "red"):
                     if (r > (b+g)*factor and r > minwaarde and (g+b) < maxwaarde):
-                        redpic.set_at((i,j),(r-(b+g),0,0))
+                        redpic.set_at((i,j),(r-(b+g)/2,0,0))
                     else:
                         redpic.set_at((i,j),(0,0,0))
                 if (color == "blue"):
                     if (b > (r+g)*factor and b > minwaarde and (r+g) < maxwaarde):
-                        redpic.set_at((i,j),(b-(r+g),0,0))
+                        redpic.set_at((i,j),(b-(r+g)/2,0,0))
                     else:
                         redpic.set_at((i,j),(0,0,0))
                 if (color == "green"):
                     if (g > (b+r)*factor and g > minwaarde and (r+b) < maxwaarde):
-                        redpic.set_at((i,j),(g-(b+r),0,0))
+                        redpic.set_at((i,j),(g-(b+r)/2,0,0))
                     else:
                         redpic.set_at((i,j),(0,0,0))
         self.pic_ball = redpic
