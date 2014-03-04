662a0d8e1944eb8ae000f929e8015f83c958fe5c
paulluuk <meneerpluk@gmail.com>
2014-03-04 12:57:54 +0100

    verbeterde "loop naar bal"

--- brain/src/behavior/plukkleur/plukkleur_0.py
+++ brain/src/behavior/plukkleur/plukkleur_0.py
@@ -55,7 +55,7 @@ class PlukKleur_0(basebehavior.behaviorimplementation.BehaviorImplementation):
                 self.nao.kijk_lager()
                 dist = self.nao.hoe_ver_bal()
                 if (dist <= 0.05):
-                    # 5 cm verder
+                    # 5 cm of minder verderop
                     self.nao.walk(0.5,0,0)
             elif (posx > -0.3 and posx < 0.3 and posy < 0):
                 print("ik zie de bal een eindje verderop.")
