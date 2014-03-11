24775d56062fb3d61e7292248fd782d462425925
asjemenao <diederikvkrieken@gmail.com>
2014-03-11 15:35:46 +0100

    - updated nao calibration

--- brain/src/util/nao_calibration.py
+++ brain/src/util/nao_calibration.py
@@ -5,7 +5,7 @@ class NaoCalibration():
     # instantiate number of measurements
     _number_of_measurements = 0
 
-    # instantiate the previous time
+    # instantiate the start time
     _start_time = time.time()
 
     # instantiate time per frame
@@ -19,14 +19,16 @@ class NaoCalibration():
         current_time = time.time()
 
         # update number of measurements when image is received
-
         if is_received:
             cls._number_of_measurements += 1
 
+        # only when there are measurements:
         # first calculate the delta between start time and current time
         # Then calculate time per frame by dividing by number of measurements
-        cls._time_per_frame = (current_time - cls._start_time) / cls._number_of_measurements
+        if cls._number_of_measurements > 0:
+            cls._time_per_frame = (current_time - cls._start_time) / cls._number_of_measurements
 
+        # print "Number of measurements: " + str(cls._number_of_measurements) + " -  time per frame: " + str(cls._time_per_frame)
 
     @classmethod
     def time_per_frame(cls):
