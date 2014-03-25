2ac7f43217966305c5703a25db7936b47bda1ad5
paulluuk <meneerpluk@gmail.com>
2014-03-25 14:34:57 +0100

    plukmain verbeteren

--- brain/src/util/balherkenning.py
+++ brain/src/util/balherkenning.py
@@ -266,7 +266,7 @@ class RasterImage:
         downleft = 0
         downright = 0
 
-        crude = 4
+        crude = 3
 
         #print("color detection - finding hotspot")
         while(crude >= 1):
