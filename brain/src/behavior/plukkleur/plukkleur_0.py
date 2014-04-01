991bf4771c25e4323062e74db87c3a0e3cb4834c
asjemenao <diederikvkrieken@gmail.com>
2014-04-01 09:55:01 +0200

    - updated project files

--- brain/src/behavior/plukkleur/plukkleur_0.py
+++ brain/src/behavior/plukkleur/plukkleur_0.py
@@ -16,40 +16,17 @@ class PlukKleur_0(basebehavior.behaviorimplementation.BehaviorImplementation):
     def implementation_init(self):
 
         self.nao = self.body.nao(0)
-        self.nao.say("Test: Pluk's Find Ball")
-        self.timer = 30
         self.nao.complete_behavior("standup")
         self.nao.zeg_dit("end_song.wav")
         self.nao.look_horizontal()
 
         self.nao.set_do_nothing_on_stop(True) # The Nao will still be enslaved
 
-        self.plukrgbfindball = self.ab.plukrgbfindball({}) # kijk waar de bal is
-        self.selected_behaviors = [
-            ("plukrgbfindball", "self.found == False")
-        ]
+        self.selected_behaviors = []
 
         self.found = False
 
     def implementation_update(self):
 
-        (posx,posy) = self.nao.waar_is_bal()
-        if (self.found == False and posx != -999 and posy != -999):
-            print("bal gevonden: stoppen met zoeken")
-            sound = random.choice(["target.wav","target2.wav","target3.wav"])
-            self.nao.walk(0.2,0,0)
-            self.nao.kijk_lager(10)
-            self.nao.zeg_dit(sound)
-            self.found = True
-            self.plukrgbfindball.set_finished()
-        elif (self.found == True and (posx == -999 or posy == -999)):
-            print("bal kwijt: verder gaan met zoeken")
-            sound = random.choice(["target_lost1.wav","target_lost2.wav"])
-            self.nao.zeg_dit(sound)
-            self.nao.walk(-0.2,0,0)
-            self.nao.kijk_hoger(10)
-            self.found = False
-            self.plukrgbfindball = self.ab.plukrgbfindball({})
-
-
-        return
+        self.nao.complete_behavior("stretch")
+        # test
